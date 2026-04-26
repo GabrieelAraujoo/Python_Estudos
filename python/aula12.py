beb68c64ba4f1454e8382542dda85c05d6db145a
GabrieelAraujoo <135062775+GabrieelAraujoo@users.noreply.github.com>
2026-04-26 14:40:58 -0300

    Add comparison operators documentation
    Added documentation for comparison operators in Python.

--- python/aula12.py
+++ python/aula12.py
@@ -1,2 +1,13 @@
+"""
+operadores de comparação (relacionais)
+OP.....Significado.....Exemplo.(True)
+>.....Maior..... 2 > 1
+>=.....Maior ou igual..... 2 >= 2
+<.....Menor..... 1 < 2 
+<=.....Menor ou igual..... 2 <= 2 
+==.....Igual.....'a' == 'a' 
+!=.....Diferente.....'a' != 'b'
+
+"""
 
 
