c2f40d744267ac417c332a3d4ac5304a5a262170
GabrieelAraujoo <135062775+GabrieelAraujoo@users.noreply.github.com>
2026-04-30 14:22:55 -0300

    Delete python directory

--- python/aula10.py
+++
@@ -1,7 +0,0 @@
-# nome = input(" qual seu nome ? ")
-# print(f"O seu nome é {nome}")
-
-numero_1 = int(input("Digite um número:"))
-numero_2 = int(input("Digite um número:"))
-
-print(f"A soma dos numeros é: {numero_1 + numero_2}")
--- python/aula5.py
+++
@@ -1,8 +0,0 @@
-concatenacao = "a" + "b" + "c"
-print(concatenacao)
-
-a_dez_vezes = "a" * 10
-tres_vezes_luiz = 3 * "Gabriel"
-print(a_dez_vezes)
-print(tres_vezes_luiz) 
- 
